9fcd7e0c12e049b8b01de863ab938e337b6c03b8
Ad <abrunet@centrale-marseille.fr>
2015-07-07 15:03:23 +0200

    using a builtin instead...

--- problem_024.py
+++ problem_024.py
@@ -8,6 +8,13 @@ A permutation is an ordered arrangement of objects. For example, 3124 is one pos
 What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 '''
 
+# If youre dumb like me and didn't kno itertools.permutation before that...
+# The builtin is just 8 times faster...
+
+import time
+
+start = time.time()
+
 
 def permute(s):
     return [s] if len(s) == 1 else [c + perm for i, c in enumerate(s) for perm in permute(s[:i] + s[i + 1:])]
@@ -24,3 +31,12 @@ test_permute()
 _list = permute('1234567890')
 _list.sort()
 print _list[999999]
+end1 = time.time()
+print end1 - start
+
+# If you're not that dumb
+from itertools import permutations
+
+print ''.join(list(permutations('0123456789', 10))[999999])
+end2 = time.time()
+print end2 - end1
