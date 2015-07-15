362bddfa2fda005ba75fb98a825d4145d0ac44b7
Ad <abrunet@centrale-marseille.fr>
2015-07-15 12:08:11 +0200

    py3 and optimizations

--- problem_001.py
+++ problem_001.py
@@ -7,20 +7,24 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 '''
 
 
-def multiple(number, _list):
-    for multiple in _list:
-        if number % multiple == 0:
-            return True
-    return False
+def multiple_from_list(_list):
+    def multiple(number):
+        for multiple in _list:
+            if number % multiple == 0:
+                return True
+        return False
+    return multiple
 
 
 def sum_multiple_of_3_5_6_9_below(number):
-    return sum([i for i in range(number) if multiple(i, [3, 5, 6, 9])])
+    return sum(filter(multiple_from_list([3, 5, 6, 9]), range(number)))
 
 
 def test_problem1():
     assert sum_multiple_of_3_5_6_9_below(10) == 23
 
 
-test_problem1()
-print sum_multiple_of_3_5_6_9_below(1000)
+if __name__ == '__main__':
+    test_problem1()
+    print(sum_multiple_of_3_5_6_9_below(1000))
+    # 233168
