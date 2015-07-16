085c17a785d20a9dfeb20a425b9c120d1bffd564
Ad <abrunet@centrale-marseille.fr>
2015-07-16 10:35:55 +0200

    Problem 48

---
+++ problem_048.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+'''
+The series, 1 ** 1 + 2 ** 2 + 3 ** 3 + ... + 10 ** 10 = 10405071317.
+
+Find the last ten digits of the series, 1 ** 1 + 2 ** 2 + 3 ** 3 + ... + 1000 ** 1000.
+'''
+
+
+def result(_max):
+    return str(sum([i ** i for i in range(1, _max + 1)]))[-10:]
+
+
+def test_result():
+    assert result(10) == '0405071317'
+
+
+if __name__ == '__main__':
+    test_result()
+    print(result(1000))
