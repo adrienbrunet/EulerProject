eabced779916a6d288ee2fa4b185e6408eec0d07
Ad <abrunet@centrale-marseille.fr>
2015-07-20 16:47:50 +0200

    Problem 35

---
+++ problem_035.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+
+'''
+The number, 197, is called a circular prime because all rotations of the digits: 197, 971, and 719, are themselves prime.
+
+There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97.
+
+How many circular primes are there below one million?
+'''
+
+
+from helpers import eratosthene
+
+
+def result(_max):
+    total = 0
+    _list = []
+    list_primes_under_one_million = [i for i in eratosthene(_max)]
+    for i in list_primes_under_one_million:
+        stri = str(i)
+        if any(number in stri for number in ['2', '4', '5', '6', '8', '0']):
+            continue
+        list_perm = {int(stri[j:] + stri[:j]) for j in range(len(stri))}
+        if all(perm >= i and perm in list_primes_under_one_million for perm in list_perm):
+            total += len(list_perm)
+            _list.append(i)
+    return 2 + total
+
+
+def test_result():
+    assert result(100) == 13
+
+
+if __name__ == '__main__':
+    test_result()
+    print(result(1000000))
+    # 55
