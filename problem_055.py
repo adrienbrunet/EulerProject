e174b00d6d0c8975ae1f08ec498f7f575350290f
Ad <abrunet@centrale-marseille.fr>
2015-07-27 09:37:14 +0200

    Problem 55

---
+++ problem_055.py
@@ -0,0 +1,51 @@
+# coding: utf-8
+
+
+'''
+If we take 47, reverse and add, 47 + 74 = 121, which is palindromic.
+
+Not all numbers produce palindromes so quickly. For example,
+
+349 + 943 = 1292,
+1292 + 2921 = 4213
+4213 + 3124 = 7337
+
+That is, 349 took three iterations to arrive at a palindrome.
+
+Although no one has proved it yet, it is thought that some numbers, like 196, never produce a palindrome. A number that never forms a palindrome through the reverse and add process is called a Lychrel number. Due to the theoretical nature of these numbers, and for the purpose of this problem, we shall assume that a number is Lychrel until proven otherwise. In addition you are given that for every number below ten-thousand, it will either (i) become a palindrome in less than fifty iterations, or, (ii) no one, with all the computing power that exists, has managed so far to map it to a palindrome. In fact, 10677 is the first number to be shown to require over fifty iterations before producing a palindrome: 4668731596684224866951378664 (53 iterations, 28-digits).
+
+Surprisingly, there are palindromic numbers that are themselves Lychrel numbers; the first example is 4994.
+
+How many Lychrel numbers are there below ten-thousand?
+
+NOTE: Wording was modified slightly on 24 April 2007 to emphasise the theoretical nature of Lychrel numbers.
+'''
+
+
+from helpers import memoize
+
+
+def reverse_and_add(number):
+    return number + int(str(number)[::-1])
+
+
+def is_palindrome(number):
+    return int(str(number)[::-1]) == number
+
+
+@memoize
+def give_palindrome_or_not(number):
+    for counter in range(50):
+        number = reverse_and_add(number)
+        if is_palindrome(number):
+            return True
+    return False
+
+
+def result():
+    return sum([1 for _ in range(10001) if not give_palindrome_or_not(_)])
+
+
+if __name__ == '__main__':
+    print(result())
+    # 249
