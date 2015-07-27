353a276e5206c8244a5e93dd03942421777541d2
Ad <abrunet@centrale-marseille.fr>
2015-07-27 09:37:43 +0200

    Problem 56

---
+++ problem_056.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+
+'''
+A googol (10 ** 100) is a massive number: one followed by one-hundred zeros; 100 ** 100 is almost unimginably large: one followed by two-hundred zeros. Despite their size, the sum of the digits in each number is only 1.
+
+Considering natural numbers of the form, ab, where a, b < 100, what is the maximum digital sum?
+'''
+
+
+def sum_digits(number):
+    return sum([int(i) for i in str(number)])
+
+
+def result():
+    _max = 0
+    for a in range(100):
+        for b in range(100):
+            if sum_digits(a ** b) > _max:
+                _max = sum_digits(a ** b)
+    return _max
+
+
+if __name__ == '__main__':
+    print(result())
+    # 972
