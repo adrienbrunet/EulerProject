31df3ff7e86c8be209d395ec9879738ae1ea18a9
Adrien Brunet <adrien@emencia.com>
2017-02-04 16:28:23 +0100

    Update problem 35 - wording

--- problem_035.py
+++ problem_035.py
@@ -8,22 +8,20 @@ There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73
 How many circular primes are there below one million?
 '''
 
-
 from helpers import eratosthene
 
 
 def result(_max):
     total = 0
-    _list = []
+    even_numbers = set('024568')
     list_primes_under_max = [i for i in eratosthene(_max)]
     for i in list_primes_under_max:
-        stri = str(i)
-        if any(number in stri for number in ['2', '4', '5', '6', '8', '0']):
+        str_i = str(i)
+        if not even_numbers.is_disjoint(str_i):
             continue
-        list_perm = {int(stri[j:] + stri[:j]) for j in range(len(stri))}
-        if all(perm >= i and perm in list_primes_under_max for perm in list_perm):
-            total += len(list_perm)
-            _list.append(i)
+        permutations = {int(str_i[j:] + str_i[:j]) for j in range(len(str_i))}
+        if all(perm >= i and perm in list_primes_under_max for perm in permutations):
+            total += len(permutations)
     return 2 + total  # we skipped 2 and 5
 
 
